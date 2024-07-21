1560ee41376e4deb289c902bf46cf1933580280c
Louie Alfonso <lfriasalfonso@gmail.com>
2024-07-21 22:03:33 +0100

    Added Login & Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,54 @@
+import { Route, Routes, Navigate, Outlet, useLocation } from 'react-router-dom'
 import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import Tasks from './pages/Task'
+import Users from './pages/Users'
+import Trash from './pages/Trash'
+import Task from './pages/Task'
+import { Toaster } from 'sonner'
+
+function Layout() {
+  const { user } = useSelector((state) => state.auth);
+  const location = useLocation();
+
+  return user ? (
+    <div className='w-full h-screen flex flex-col md:flex-row'>
+      <div className='w-1/5 h-screen bg-white sticky top-0 hidden md:block'>
+        {/* <Sidebar /> */}
+      </div>
+      <MobileSidebar />
+      <div className='flex-1 overflow-y-auto'>
+        {/*<Navbar />*/}
+        <div className='p-4 2xl:px-10'>
+          {/* <Outlet /> */}
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Navigate to='/log-in' state={{ from: location }} replace />
+  );
+}
+
 
 function App() {
-
   return (
-    <>
-      <Login/>
-    </>
+    <main className='w-full min-h-screen bg-[#f3f4f6] '>
+        <Routes>
+          <Route element={<Layout/>}>
+            <Route path='/' element={<Navigate to='/dashboard'/>}/>
+            <Route path='/dashboard' element={<Dashboard/>}/>
+            <Route path='/tasks' element={<Tasks/>}/>
+            <Route path='/completed/:status' element={<Tasks/>}/>
+            <Route path='/in-progress/:status' element={<Tasks/>}/>
+            <Route path='/todo/:status' element={<Tasks/>}/>
+            <Route path='/team' element={<Users/>}/>
+            <Route path='/trash' element={<Trash/>}/>
+           <Route path='/task/:id' element={<Task/>}/> 
+          </Route>
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+        <Toaster richColors/>
+    </main>
   )
 }
 
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,78 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useForm } from 'react-hook-form'
+import { useNavigate } from "react-router-dom";
+import Textbox from "../components/Textbox";
+import Button from "../components/Button";
+
 
 const Login = () => {
+  const user  = "";
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const navigate = useNavigate();
+
+  const submitHandler = async (data) => {
+    console.log("submit");
+  };
+
+  useEffect(() => {
+    user && navigate("/dashboard");
+  }, [user]);
+
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <div className='w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]'>
+      <div className='w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center'>
+        <div className='w-full md:w-3/3 p-4 md:p-1 flex flex-col justify-center items-center'>
+          <form onSubmit={handleSubmit(submitHandler)} className='form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-white px-10 pt-14 pb-14'>
+            <div className=''>
+              <p className='text-blue-600 text-3xl font-bold text-center'>
+                Task Manager App
+              </p>
+              <p className='text-center text-base text-gray-700 '>
+                Keep all your credential safe.
+              </p>
+            </div>
+            <div className='flex flex-col gap-y-5'>
+              <Textbox
+                placeholder='email@example.com'
+                type='email'
+                name='email'
+                label='Email Address'
+                className='w-full rounded-full'
+                register={register("email", {
+                  required: "Email Address is required!",
+                })}
+                error={errors.email ? errors.email.message : ""}
+              />
+              <Textbox
+                placeholder='your password'
+                type='password'
+                name='password'
+                label='Password'
+                className='w-full rounded-full'
+                register={register("password", {
+                  required: "Password is required!",
+                })}
+                error={errors.password ? errors.password.message : ""}
+              />
+              <span className='text-sm text-gray-500 hover:text-blue-600 hover:underline cursor-pointer'>
+                Forget Password?
+              </span>
+
+              <Button
+                type='submit'
+                label='Submit'
+                className='w-full h-10 bg-blue-700 text-white rounded-full'
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
